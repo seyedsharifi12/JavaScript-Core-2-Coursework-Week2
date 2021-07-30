59e7f19bc2406c06b13326287ee68c414661617e
seyedsharifi12 <seyedsharifi12@gmail.com>
2021-07-31 00:42:15 +0100

    started on adanced

--- advanced/1-guess-the-number-project/script.js
+++ advanced/1-guess-the-number-project/script.js
@@ -1,9 +1,23 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+let gamepPlay = true;
+let gameOver = false;
+let triesAllowed = 7;
+let triesTaken = 0;
+let numberOfTries;
 
 function guessNumber() {
   //Collect input from the user
   let guess = document.querySelector(".inputs-Values").value;
-
+  const print = document.querySelector(".final-output");
+  const triesUsed = document.querySelector(".Tries-output");
+  if (guess === "" || guess > 100 || guess <= 0) {
+    print.innerText = "Please enter a number between 1 and 100";
+  } else if (guess > randomNumber) {
+    triesAllowed -= 1;
+    triesTaken += 1;
+    print.innerText = "Number is too high, try again";
+    numberOfTries = triesUsed.innerText = `Number of tries:  ${triesAllowed}`;
+  }
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
 
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
